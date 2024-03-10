da81996669a4990d62728c14432881e28cff5348
Hayjk <tosts71267@gmail.com>
2024-03-10 17:13:29 +0300

    10.03.2024 17:13 comit2

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-12o2=4cekzod-p%-eqf1&ont-wa5&)um!sj4w!repoc()zg)z8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com', 'hayjk.pythonanywhere.com']
 
 
 # Application definition
